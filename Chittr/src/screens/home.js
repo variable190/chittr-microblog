e104358e92bfd59f346b315235a5488c1a6bd8df
drHazRuns <djmhayes@protonmail.com>
2020-03-06 21:58:19 +0000

    add networking to home.js

--- Chittr/src/screens/home.js
+++ Chittr/src/screens/home.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react'
 import { ScrollView } from 'react-native'
 import Chit from '../components/chit'
+import fetch from 'node-fetch'
 
 export default class HomeScreen extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      start: '0',
+      count: '10',
+      chitts: []
+    }
+  }
+
+  componentDidMount () {
+    this.getChits()
+  }
+
+  getChits () {
+    return fetch('http://192.168.0.4:3333/api/v0.0.5/chits?' +
+      `start=${this.state.start}&count=${this.state.count}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Authorization': `${this.props.token}`
+      }
+    })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ json })
+      },
+      err => {
+        console.log(err.name)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
   render () {
+    return this.renderJson()
+  }
+
+  renderJson () {
+    const contents = this.state.chitts.map((chit) => {
+      // eslint-disable-next-line no-unused-expressions
+      <Chit user={chit.user.given_name} chit={chit.chit_content} />
+    })
     return (
       <ScrollView>
-        <Chit user='user 1' chit='if i keep typing for a very very long time then this chit will definitely be over 141 characters. That sentence was 97 in total but I am hopi' />
-        <Chit user='user 2' chit='if i keep typing for a very very long time then this chit will definitely be over 141 characters. That sentence was 97 in total but I am hopi' />
-        <Chit user='user 3' chit='if i keep typing for a very very long time then this chit will definitely be over 141 characters. That sentence was 97 in total but I am hopi' />
-        <Chit user='user 4' chit='if i keep typing for a very very long time then this chit will definitely be over 141 characters. That sentence was 97 in total but I am hopi' />
+        {contents}
       </ScrollView>
     )
   }
